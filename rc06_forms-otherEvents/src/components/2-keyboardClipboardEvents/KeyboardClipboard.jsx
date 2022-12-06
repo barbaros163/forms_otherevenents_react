617b4499db360b0638c6c37b86f8f54213be7176
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-06 17:59:28 +0100

    onkeydown

---
+++ rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const KeyboardClipboard = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+      alert("Please dont enter a number");
+      e.preventDefault();
+    }
+  };
+  return (
+    <div className="container text-center">
+      <h1>CLIPBOARD EVENTS</h1>
+      <input
+        className="form-control"
+        type="text"
+        onChange={(e) => {
+          e.target.value = e.target.value.toLocaleUpperCase();
+          setInputValue(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
+      />
+
+      <p className="text-start mt-4">{inputValue}</p>
+    </div>
+  );
+};
+
+export default KeyboardClipboard;
